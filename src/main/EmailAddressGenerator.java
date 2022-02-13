42c55ccfb9a26db84ee9d7ff5e0b0ba7ad88d82b
0x22ff0c <markandydizon@gmail.com>
2022-02-13 19:53:05 +0800

    Update project
    - Created EmailAddressGenerator class
    - Moved the following methods to the class mentioned:
    	- setDomainName
    	- generateEmailName
    	- generateEmailAddress
    - Moved emailName class variable to the class mentioned.

---
+++ src/main/EmailAddressGenerator.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class EmailAddressGenerator {
+
+	private String emailName = "";
+	
+	public String setDomainName(){
+		
+		System.out.print("Enter a domain name: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String domainName = scanner.nextLine();
+		
+		scanner.close();
+		
+		return domainName;
+		
+	}
+	
+	public void generateEmailName(){
+		
+		Random random = new Random();
+		
+		String[] charArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+				"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+				"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+				"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "k", "l", "m",
+				"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for(int index = 0; index <= 10; index++){
+			
+			stringBuilder.append(charArray[random.nextInt(charArray.length)]);
+			
+		}
+		
+		emailName = stringBuilder.toString();
+		
+		
+	}
+	
+	public void generateEmailAddress(String domainName){
+		
+		System.out.println(String.format("Generated email address: %s", emailName + "@" + domainName));
+		
+	}
+	
+	
+}
